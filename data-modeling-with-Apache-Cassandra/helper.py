24ef9a24e6f83115d44ed2e4c29b3897967f2b92
Heschmat <heschmat.seyedi@gmail.com>
2021-04-07 13:17:32 +0200

    Seperate drop & create; Inform user when performing actions on tables.

--- data-modeling-with-Apache-Cassandra/helper.py
+++ data-modeling-with-Apache-Cassandra/helper.py
@@ -2,24 +2,28 @@ import csv
 import cassandra
 
 # Helper functions to drop and create the table.
-def drop_create_table(session, table_name, query):
-    """Drop and create the table.
+def drop_table(session, table_name):
+    """Drop he table if exists.
     :table_name {str}
-    :query {str}
     """
     # Drop the table if it exits.
     try:
         session.execute(f"Drop TABLE IF EXISTS {table_name};")
-        print(f'>>> Dropped {table_name} with success.')
+        print(f'>>> Dropped <{table_name}> with success.')
     except Exception as e:
-        print(f'>>> Could not drop {table_name};\n{e}')
+        print(f'>>> Could not drop <{table_name}>;\n{e}')
+
         
-    # Create the table if it doesn't exist.
+def create_table(session, table_name, query):
+    """ Create the table if it doesn't exist.
+    :table_name {str}
+    :query {str}
+    """
     try:
         session.execute(query)
-        print(f'>>> Table {table_name} created!')
+        print(f'>>> Table <{table_name}> created!')
     except Exception as e:
-        print(f'>>> Could not create {table_name};\n{e}')
+        print(f'>>> Could not create <{table_name}>;\n{e}')
 
         
 def populate_table(session, file_path, query, columns):
@@ -35,6 +39,7 @@ def populate_table(session, file_path, query, columns):
     
     # Read the csv file, and populate the table.
     with open(file_path, encoding = 'utf8') as f:
+        print('>>> Populating the table.')
         csvreader = csv.reader(f)
         # Skip the header.
         next(csvreader)
@@ -46,3 +51,4 @@ def populate_table(session, file_path, query, columns):
             # Also, convert each value to the appropriate format,
             # based on the table.
             session.execute(query, (col_types[i](line[i]) for i in columns))
+        print('Done.')
